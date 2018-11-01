c3eb009ba3f293322980757ec465a6ed13327f81
Cody Brooks <cbrooks@seismic.com>
2018-10-31 19:26:01 -0700

    Removed lazy component as a submodule which happened by mistake

---
+++ cody-test-child-lazy-module/src/LazyLoadedComponent/LazyLoadedComponent.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class LazyLoadedComponent extends React.Component {
+    render() {
+        return (
+            <div>This is a lazy loaded component!</div>
+        );
+    }
+}
---
+++ cody-test-child-lazy-module/src/entry.js
@@ -0,0 +1,5 @@
+import {LazyLoadedComponent} from "./LazyLoadedComponent/LazyLoadedComponent";
+
+export {
+    LazyLoadedComponent,
+};
